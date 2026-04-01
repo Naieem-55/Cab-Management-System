edb3b782e8334678742fa972dcabda3e2ae23059
Naieem Islam <naieemislam.opl@gmail.com>
2026-04-01 11:29:43 +0600

    added User Role enum

---
+++ Models/Enums/UserRole.cs
@@ -0,0 +1,11 @@
+namespace Cab_Management_System.Models.Enums
+{
+    public enum UserRole
+    {
+        Admin,
+        FinanceManager,
+        HRManager,
+        TravelManager,
+        Customer
+    }
+}
